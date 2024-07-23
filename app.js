82d3dbe13f5dcee42aa2b03e4dfd98806e9b7314
EC2 Default User <ec2-user@ip-172-31-8-165.ap-northeast-1.compute.internal>
2024-07-23 05:47:47 +0000

    【first commit】JavaScript練習

---
+++ app.js
@@ -0,0 +1,10 @@
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い')
+} else if(orange == apple){
+  alert('みかんとりんごの値段が同じ')
+} else if(orange > apple){
+  alert('みかんの値段がりんごより高い')
+}
